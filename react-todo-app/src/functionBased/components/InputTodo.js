73f6c1e93044eafbfa7111e942534b358a95ca4d
Jude Harding <jharding@genesisdla.com>
2021-04-21 16:43:12 -0400

    useState hooks

--- react-todo-app/src/functionBased/components/InputTodo.js
+++ react-todo-app/src/functionBased/components/InputTodo.js
@@ -1,3 +1,4 @@
+// importing the useState for hooks
 import React, { useState } from "react";
 
 // https://ibaslogic.com/react-hooks-tutorial/
@@ -6,26 +7,58 @@ import React, { useState } from "react";
 const InputTodo = (props) => {
 	// HOOK
 	const [inputText, setInputText] = useState({
-		fName: "",
-		lastName: "",
+		// will use the spread operator to access data below
+		// fName: "",
+		// lastName: "",
+		title: "",
 	});
 
+	// onChange handles input from the user
 	const onChange = (e) => {
 		setInputText({
+			// spread operator allows for multiple entries
+			// into the useState object
 			...inputText,
 			[e.target.name]: e.target.value,
 		});
 	};
 
+	// prevState can be used for check boxes and radio buttons
+	// const onChange = (e) => {
+	// 	setInputText((prevState) => {
+	// 		return {
+	// 			...prevState,
+	// 			[e.target.name]: e.target.value,
+	// 		};
+	// 	});
+	// };
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("submitted");
+		if (inputText.title.trim()) {
+			props.addTodoProps(inputText.title);
+			setInputText({
+				title: "",
+			});
+		} else {
+			alert("Please write item");
+		}
 	};
 
 	return (
 		<>
 			<form onSubmit={handleSubmit} className="form-container">
 				<input
+					type="text"
+					className="input-text"
+					placeholder="Add todo..."
+					value={inputText.title}
+					name="title"
+					onChange={onChange}
+				/>
+
+				{/* MULTIPLE FIELDS  */}
+				{/* <input
 					type="text"
 					className="input-text"
 					placeholder="Add first name"
@@ -40,7 +73,7 @@ const InputTodo = (props) => {
 					value={inputText.lastName}
 					name="lastName"
 					onChange={onChange}
-				/>
+				/> */}
 
 				<button className="input-submit">Submit</button>
 			</form>
