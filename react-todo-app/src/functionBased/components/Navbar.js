dca628ad5a54e84c5f67d6c438094f088d25598b
Jude Harding <jharding@genesisdla.com>
2021-04-26 16:04:32 -0400

    finished ibaslogic w/ hamburger menu icon

--- react-todo-app/src/functionBased/components/Navbar.js
+++ react-todo-app/src/functionBased/components/Navbar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+	const [navbarOpen, setNavbarOpen] = useState(false);
+
 	const links = [
 		{
 			id: 1,
@@ -20,14 +24,42 @@ const Navbar = () => {
 		},
 	];
 
+	const handleToggle = () => {
+		setNavbarOpen((prev) => !prev);
+	};
+
+	// Don't use this b/c you might have a state problem
+	// const handleToggle = () => {
+	// 	setNavbarOpen(!navbarOpen);
+	// };
+
+	const closeMenu = () => {
+		setNavbarOpen(false);
+	};
+
 	return (
 		<nav className="navBar">
-			<ul>
+			{/* <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button> */}
+
+			<button onClick={handleToggle}>
+				{navbarOpen ? (
+					<MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+				) : (
+					<FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+				)}
+			</button>
+
+			<ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
 				{links.map((link) => {
 					return (
 						<li key={link.id}>
 							{/* // cannot use ahref b/c it refreshes the page - use LINK or NAVLINK */}
-							<NavLink to={link.path} activeClassName="active-link" exact>
+							<NavLink
+								to={link.path}
+								activeClassName="active-link"
+								onClick={() => closeMenu()}
+								exact
+							>
 								{link.text}
 							</NavLink>
 						</li>
